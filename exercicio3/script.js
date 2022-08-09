de188bf17718395e31e819011cbc921bcec78788
GlauberBispo <glauber.bispo@gmail.com>
2022-08-09 20:46:20 -0300

    Entrega exercicio3

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,4 +1,7 @@
 //código a ser reescrito
+const nome = prompt('Qual o seu nome?');
+const idade = +prompt('Qual a sua idade?');
+
 
 if(nome === "José"){
 	console.log("Oi, Zé!");
@@ -11,3 +14,9 @@ if(idade >= 18){
 } else {
 	console.log("Ainda não pode tirar carteira de motorista");
 }
+
+// Código com ifternário
+
+nome === 'José' ? console.log('Oi, Zé!') : console.log('Olá, '+nome);
+
+idade >= 18 ? console.log("pode tirar carteira de motorista!") : console.log("Ainda não pode tirar carteira de motorista");
